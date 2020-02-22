1741eb99b0b93978fe180827e725233d4f7466ad
Rémi Vanicat <vanicat@debian.org>
2020-02-22 08:02:55 +0100

    smaller screen

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
+/* exported create */
+
 var config = {
     type: Phaser.AUTO,
-    width: 1024,
-    height: 768,
+    width: 870,
+    height: 550,
     physics: {
         default: 'arcade',
         arcade: {
@@ -27,10 +29,10 @@ function create ()
     this.add.image(config.width/2, config.height/2, 'background')
     this.gameitems = this.physics.add.group()
     
-    for (var i = 0; i < 30; i++) {
-		var x = Phaser.Math.RND.between(config.width/3, 2*config.width/3);
-		var y = Phaser.Math.RND.between(0, config.height/4);
+    for (let i = 0; i < 30; i++) {
+		let x = Phaser.Math.RND.between(config.width/3, 2*config.width/3);
+		let y = Phaser.Math.RND.between(0, config.height/4);
 
-        var newobj = this.gameitems.create(x, y, 'fish');
+        let newobj = this.gameitems.create(x, y, 'fish');
 	}
 }
